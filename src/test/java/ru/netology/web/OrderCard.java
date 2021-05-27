a11d833e1829a9822e15f630f6231b4ca0c2ac05
Anastasia <matr-nastya@yandex.ru>
2021-05-27 20:06:56 +0300

    Initial commit

---
+++ src/test/java/ru/netology/web/OrderCard.java
@@ -0,0 +1,35 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class OrderCard {
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+    @Test
+    void shouldTest() {
+        $("[data-test-id=name] input").setValue("Василий");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldTestNoName() {
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $(".input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+}
+
